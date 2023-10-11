a773cabe53cf2eca3f5ad8096303fceb0534c585
Owusu Kenneth <59415541+Owusu1946@users.noreply.github.com>
2023-10-11 06:45:32 -0800

    Update pinecone.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,12 @@
-import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeClient } from '@pinecone-database/pinecone'
 
-export const pinecone = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-west1-gcp-free',
-});
+  export const getPineconeClient = async () => {
+    const client = new PineconeClient()
+  
+    await client.init({
+      apiKey: process.env.PINECONE_API_KEY!,
+      environment: 'us-west1-gcp-free',
+    })
+  
+    return client
+  }
