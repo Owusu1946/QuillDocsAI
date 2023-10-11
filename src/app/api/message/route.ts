bfa65065267a7fa7eb3c9412a8df922d15dec347
Owusu Kenneth <59415541+Owusu1946@users.noreply.github.com>
2023-10-11 06:50:06 -0800

    Update route.ts

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -50,7 +50,7 @@ export const POST = async (req: NextRequest) => {
   })
 
   const pinecone = await getPineconeClient()
-  const pineconeIndex = pinecone.Index('quill')
+  const pineconeIndex = pinecone.Index('quillreader')
 
   const vectorStore = await PineconeStore.fromExistingIndex(
     embeddings,
